8daae8f7ca74f413f57d164b77ba5ec8c6fe7dd7
Brea Renee <brearenee@gmail.com>
2023-09-17 18:54:34 -0600

    List of edged done. TreeMap of nodes and indexes created.

--- Mst.java
+++ Mst.java
@@ -1,38 +1,92 @@
 
 import graphinput.Edge;
 import graphinput.File2Edges;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
+
+
 public class Mst {
     static final String DEF_FILE_NAME = "town.txt";
-    private final String dataFileName;
+
     public static void main(String[] args) {
-        Mst mst;
+        Mst mst = new Mst();
         if (args.length == 1) {
-            mst = new Mst(args[0]);
+            mst.getMst(args[0]);
         } else {
-            mst = new Mst(DEF_FILE_NAME);
+            mst.getMst(DEF_FILE_NAME);
         }
-    }
-    public Mst(String filename) {
-        dataFileName = filename;
     }
 
     public Mst() {
-        this(DEF_FILE_NAME);
     }
 
-    private void getMst(String filename){
+    private static void getMst(String filename) {
+        List<Edge> graphEdges = File2Edges.getEdgesFromFile(filename);
+        List<Edge> sortedEdges = new ArrayList<>();
+        sortedEdges = getSortedEdges(graphEdges);
+        System.out.println("sorted Edges: ");
+        for (Edge edge: sortedEdges){
+            System.out.println(edge.toString());
+        }
+        TreeMap<String, Integer> nodeKey = createNodeKey(sortedEdges);
+        System.out.println("new nodeKey:" );
 
+        for (Map.Entry<String, Integer> entry : nodeKey.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("Key: " + key + ", Value: " + value);
+        }
+    }
+
+    private static List<Edge> getSortedEdges(List<Edge> unsortedList) {
+
+        List<Edge> sortedEdges = new ArrayList<>();
+        Edge smallest = unsortedList.get(0);
+        int size = unsortedList.size();
+
+        int i = 0;
+        while (i < size) {
+            smallest = unsortedList.get(0);
+
+            for (Edge edge : unsortedList) {
+                int edgeWeight = edge.weight();
+                int smallestWeight = smallest.weight();
+                if (edgeWeight <= smallestWeight) smallest = edge;
+            }
+            sortedEdges.add(smallest);
+            unsortedList.remove(smallest);
+            i++;
+        }
+        return sortedEdges;
     }
 
 
+    private static TreeMap<String, Integer> createNodeKey (List<Edge> sortedList){
+        TreeMap<String, Integer> keyMap = new TreeMap<>();
+        String node;
+        int count = 0;
+        Integer duplicate;
 
+        for (Edge edge : sortedList){
+            node = edge.start();
+            if (!keyMap.containsKey(node)) {
+                keyMap.put(node,count++);
 
+            }
+            node = edge.end();
+            if (!keyMap.containsKey(node)) {
+                keyMap.put(node,count++);
+            }
+        }
+        //I wanted node A:0 , B:1, C:2 just for my own sake, so I'm reordering
+        count = 0;
+        for (Map.Entry<String, Integer> entry : keyMap.entrySet()) {
+            String key = entry.getKey();
+            keyMap.put(key, count++);
+        }
 
-
+        return keyMap;
+    }
 }
-
 
 
 
